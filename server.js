40b9999cb26314947113753d5db103db8e6191e3
alan murphy <alanmurphy@alans-MacBook-Pro.local>
2023-08-31 11:47:50 +0200

    add second server and artillery

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require('path');
 const pidusage = require('pidusage');
 
 const app = express();
+const app2 = express();
 const port = 3000;
+const port2 = 4000;
 
 // Serve HTML files
 app.get('/', (req, res) => {
@@ -15,7 +17,7 @@ app.get('/api/server-info', (req, res) => {
     cpuIntensiveTask(100);  // 5 seconds of CPU intensive task
     pidusage(process.pid, function (err, stats) {
       const serverInfo = {
-        Type: 'Express Server',
+        Type: 'Express Server 1',
         Language: 'JavaScript',
         Port: port,
         'Current Server Time': new Date().toLocaleString(),
@@ -29,6 +31,34 @@ app.get('/api/server-info', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+
+// Serve HTML files
+app2.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Provide server information via API
+app2.get('/api/server-info', (req, res) => {
+    cpuIntensiveTask(100);  // 5 seconds of CPU intensive task
+    pidusage(process.pid, function (err, stats) {
+      const serverInfo = {
+        Type: 'Express Server 2',
+        Language: 'JavaScript',
+        Port: port,
+        'Current Server Time': new Date().toLocaleString(),
+        'CPU usage': stats.cpu
+      };
+  
+      res.json(serverInfo);
+    });
+});
+
+app2.listen(port2, () => {
+  console.log(`Server running at http://localhost:${port2}/`);
+});
+
+
 
 // Function to perform CPU intensive task for durationMs milliseconds
 function cpuIntensiveTask(durationMs) {
