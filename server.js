ab65553c5636d469695ed02da0659fa82b9729d2
alan murphy <alanmurphy@alans-MacBook-Pro.local>
2023-08-31 11:07:16 +0200

    add expensive endpoint

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const pidusage = require('pidusage');
+
 const app = express();
 const port = 3000;
 
@@ -10,16 +12,28 @@ app.get('/', (req, res) => {
 
 // Provide server information via API
 app.get('/api/server-info', (req, res) => {
-  const serverInfo = {
-    Type: 'Express Server',
-    Language: 'JavaScript',
-    Port: port,
-    'Current Server Time': new Date().toLocaleString()
-  };
-
-  res.json(serverInfo);
+    cpuIntensiveTask(100);  // 5 seconds of CPU intensive task
+    pidusage(process.pid, function (err, stats) {
+      const serverInfo = {
+        Type: 'Express Server',
+        Language: 'JavaScript',
+        Port: port,
+        'Current Server Time': new Date().toLocaleString(),
+        'CPU usage': stats.cpu
+      };
+  
+      res.json(serverInfo);
+    });
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+// Function to perform CPU intensive task for durationMs milliseconds
+function cpuIntensiveTask(durationMs) {
+  const end = Date.now() + durationMs;
+  while (Date.now() < end) {
+    Math.sqrt(Math.random());
+  }
+}
